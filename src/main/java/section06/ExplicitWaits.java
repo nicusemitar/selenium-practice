4df838e4ec512fa5151e824dc65c118cb5b7a095
nicolae.semitar <nicolae.semitar@eu.rsystems.com>
2023-11-29 18:37:30 +0200

    Section 6 - Waits part2

--- src/main/java/section06/ExplicitWaits.java
+++ src/main/java/section06/ExplicitWaits.java
@@ -3,22 +3,22 @@ package section06;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ExplicitWaits {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\nicolae.semitar\\OneDrive - R Systems\\Documents\\Selenium\\chromedriver-win32\\chromedriver.exe");
-        WebDriver webDriver = new ChromeDriver();
 
+        WebDriver webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         webDriver.get("https://automationtesting.co.uk/loader.html");
 
-        webDriver.findElement(By.cssSelector("[onclick]")).click();
+        WebDriverWait wait = new WebDriverWait(webDriver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button#loaderBtn")));
 
-        Thread.sleep(2000);
+        webDriver.findElement(By.cssSelector("button#loaderBtn")).click();
         webDriver.quit();
     }
 }
